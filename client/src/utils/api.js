37227e5ab0eaad8b6b54d2d5858dd9432d9ceb07
Mike Fedele <mikefid11@gmail.com>
2022-06-03 23:52:41 -0400

    cors-cure server works

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,14 +1,14 @@
 // const yelp = require('yelp-fusion');
-const CORS = "https://cors-anywhere.herokuapp.com/"
-
+// const CORS = "https://cors-anywhere.herokuapp.com/"
+// const CORS = "https://cors.bridged.cc/"
 // Yelp needed cors-anywhere until deployed. Can update after deployment
 // cors addded
 
 // add .env to client folder to hide API key
-// todo cors change
+//* CORS-CURE server on heroku
 export const searchYelpApi = (query) => {
   console.log('hitAPI')
-    return fetch(`${CORS}https://api.yelp.com/v3/businesses/search?term=dogs_allowed&location=${query}`, {
+    return fetch(`https://evening-mesa-85661.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=dogs_allowed&location=${query}`, {
       method: 'GET',
       headers: {
         authorization: process.env.REACT_APP_API_KEY,
@@ -16,3 +16,4 @@ export const searchYelpApi = (query) => {
     });
   };
 
+   // authorization: 'Bearer sFDrQ2pCQos8PDSDDwhIcVDKCUPVRHBWQf8OUcjX3PKW-d6e0S_uxIlVXXbHGFf96nF8w-VqDPw_2ZzSU-5-ievJLv_YlGpOQkamfNYe3l5k3b0BnlP2gTXQ5ZyLYnYx',
